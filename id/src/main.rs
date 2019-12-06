6dd9ac6d52a5537563c80efaa64153322ce15657
Satyendra Yadav <58596546+satyendra-ops@users.noreply.github.com>
2019-12-06 23:35:33 +0530

    Update main.rs

--- id/src/main.rs
+++ id/src/main.rs
@@ -106,7 +106,51 @@ return userid;
 
 }}
 
+//function to get uid from username in case of user entered username
+//uses libc function getpwnam_r which is thread-safe
+fn get_unix_gid<S: AsRef<OsStr> + ?Sized>(username: &S) -> u32 
+{
+    
+    let username = match CString::new(username.as_ref().as_bytes()) 
+    {
+        Ok(u)  => u,
+        Err(_) => {
+            // The username that was passed in contained a null character,
+            // which will match no usernames.
+            return 999;
+        }
+    };
+    
+    unsafe 
+	{
+	
+        let mut result = ptr::null_mut();
+        let amt = match libc::sysconf(libc::_SC_GETPW_R_SIZE_MAX) 
+		{
+            		n if n < 0 => 512 as usize,
+            		n => n as usize,
+        	};
+        let mut buf = Vec::with_capacity(amt);
+        let mut passwd: libc::passwd = mem::zeroed();
 
+        libc::getpwnam_r(username.as_ptr(), &mut passwd, buf.as_mut_ptr(), buf.capacity() as libc::size_t, &mut result);
+    
+	if result.is_null() {
+        // There is no such user, or an error has occurred.
+        // errno gets set if there’s an error.
+        return 999;
+    }
+
+        if result != &mut passwd {
+        // The result of getpwnam_r should be its input passwd.
+        return 999;
+    }
+	
+	let userid=(*result).pw_gid;
+	//println!("{}",userid);
+return userid;
+
+}}
 	
 fn main() {
 	let args: Vec<String> = env::args().collect();
@@ -128,21 +172,26 @@ fn main() {
 	}
 	
 
-    	unsafe{ let id;
+    	unsafe{ let id;let gid;
 		if j!=9999
 		{
 			name=&args[j];
-			println!("{}",name);
+			//println!("{}",name);
 			id = get_unix_uid(name);
+			gid = get_unix_gid(name);
 		}
 		else
 		{
 			id=libc::geteuid();
+			gid=libc::getegid();
 		}
-         	let gid=libc::getegid();
 		let uname = get_unix_username(id).unwrap();
 		let gname = get_unix_groupname(gid).unwrap();
 		if n==1
+		{
+			println!("uid={}({}) gid={}({})",id,uname,gid,gname);
+		}
+		if n==2 && gflag==0 && uflag==0
 		{
 			println!("uid={}({}) gid={}({})",id,uname,gid,gname);
 		}
